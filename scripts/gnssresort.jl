0575e3271609de83a0da91804a39edc607ec64cc
Nathanael Wong <natgeo.wong@outlook.com>
2020-06-28 01:26:40 -0400

    Resort GNSS completed!!  To do on cluster!

--- scripts/gnssresort.jl
+++ scripts/gnssresort.jl
@@ -2,3 +2,13 @@ using DrWatson
 @quickactivate "MaritimeContPWV"
 
 include(srcdir("gnss.jl"))
+
+function gnssresortall()
+
+    allinfo = retrieveginfo(); stns = allinfo[:,1]
+    for stn in stns; gstationresort(gstationinfo(stn,allinfo)) end
+
+end
+
+# gnssresortall()
+for stn in ["NTUS","SING"]; gstationresort(gstationinfo(stn,retrieveginfo())) end
--- src/gnss.jl
+++ src/gnss.jl
@@ -3,6 +3,19 @@ using DelimitedFiles
 using Logging
 using NCDatasets
 
+function ncoffsetscale(data::Array{<:Real})
+
+    dmax = maximum(data); dmin = minimum(data);
+    scale = (dmax-dmin) / 65533;
+    offset = (dmax+dmin-scale) / 2;
+
+    return scale,offset
+
+end
+
+yr2str(date::TimeType)   = Dates.format(date,dateformat"yyyy")
+yrmo2str(date::TimeType) = Dates.format(date,dateformat"yyyymm")
+
 function stationvalid(dateint::Integer)
 
     yr = floor(dateint/10000)
@@ -16,7 +29,7 @@ function stationvalid(dateint::Integer)
 
 end
 
-function retrieveinfo()
+function retrieveginfo()
 
     fdir = srcdir("SuGAr.sites")
     @info "$(Dates.now()) - Retrieving SuGAr station info from file $(fdir)"
@@ -29,7 +42,7 @@ function retrieveinfo()
 
 end
 
-function stationinfo(stn::AbstractString,allinfo::AbstractArray)
+function gstationinfo(stn::AbstractString,allinfo::AbstractArray)
 
     stninfo = Dict{AbstractString,Any}()
 
@@ -45,16 +58,100 @@ function stationinfo(stn::AbstractString,allinfo::AbstractArray)
 
 end
 
-function stationresort(sinfo::AbstractDict)
+function gstationresort(sinfo::AbstractDict)
 
     dbeg = sinfo["start"]; yrbeg = Year(dbeg); mobeg = Month(dbeg)
-    dend = sinfo["start"]; yrend = Year(dend); moend = Month(dend)
+    dend = sinfo["end"]; yrend = Year(dend); moend = Month(dend)
     dvec = DateTime(yrbeg,mobeg) : Month(1) : DateTime(yrend,moend);
+
+    gdata = readdlm(datadir("gnss/$(sinfo["name"]).tdpzwd"),Any,comments=true);
+    gdate = collect(DateTime(2000,1,1,12,0,0) .+ Second.(@view gdata[:,2]));
+    gyear = Dates.year.(gdate); gmonth = Dates.month.(gdate);
 
     for id in dvec
 
-        dtvec = id : Minute(10) : (id + Dates.Month(1))
+        iyear = Dates.year(id); imonth = Dates.month(id)
+        dtvec = collect(id : Minute(10) : (id + Dates.Month(1))); pop!(dtvec)
+        idata = @view gdata[(gyear .== iyear) .& (gmonth .== imonth),3:4]
+        idate = @view gdate[(gyear .== iyear) .& (gmonth .== imonth)]
+
+        ndy = daysinmonth(id); ii = 0; ei = 0;
+        zdata = zeros(144,ndy); sdata = zeros(144,ndy);
+
+        for idt in dtvec; ii += 1
+
+            jj = findfirst(isequal.(idt,idate));
+            if isnothing(jj)
+                  zdata[ii] = NaN; sdata[ii] = NaN; ei += 1;
+            else; zdata[ii] = idata[jj,1]; sdata[ii] = idata[jj,2]
+            end
+
+        end
+
+        if ei != length(dtvec)
+            gstationsave(zdata,sdata,dtvec,sinfo)
+        else
+            @info "$(Dates.now()) - No valid ZWD data is available from the $(sinfo["name"]) station in $(monthname(id)) $(year(id))"
+        end
 
     end
+
+end
+
+function gstationsave(
+    zwd::AbstractArray, sig::AbstractArray, dt::AbstractArray,
+    sinfo::AbstractDict
+)
+
+    fol = datadir("gnss/$(sinfo["name"])/$(yr2str(dt[1]))");
+    if !isdir(fol); mkpath(fol); end
+
+    fnc = datadir("$fol/$(sinfo["name"])-$(yrmo2str(dt[1])).nc")
+    if isfile(fnc)
+        @info "$(Dates.now()) - Stale NetCDF file $(fnc) detected.  Overwriting ..."
+        rm(fnc);
+    end
+
+    ds = NCDataset(fnc,"c",attrib = Dict(
+        "Conventions"  => "CF-1.6",
+        "Date Created" => "$(Dates.now())"
+    ))
+
+    ds.dim["coordinate"] = 1
+    ds.dim["step"] = 144
+    ds.dim["days"] = size(zwd,2)
+    ds.dim["time"] = size(zwd,2) * 144
+
+    nclon = defVar(ds,"longitude",Float64,("coordinate",),attrib = Dict(
+        "units"     => "degrees_east",
+        "long_name" => "longitude",
+    ))
+
+    nclat = defVar(ds,"latitude",Float64,("coordinate",),attrib = Dict(
+        "units"     => "degrees_north",
+        "long_name" => "latitude",
+    ))
+
+    nctime = defVar(ds,"time",Int32,("time",),attrib = Dict(
+        "units"     => "minutes since $(Date(dt[1])) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian",
+    ))
+
+    nczwd = defVar(ds,"zwd",Float32,("step","days"),attrib = Dict(
+        "long_name"     => "zenith_wet_delay",
+        "full_name"     => "Zenith Wet Delay",
+        "units"         => "m",
+    ))
+
+    ncsig = defVar(ds,"sigma",Float32,("step","days"),attrib = Dict(
+        "long_name"     => "uncertainty_zenith_wet_delay",
+        "full_name"     => "Uncertainty in Zenith Wet Delay",
+        "units"         => "m",
+    ))
+
+    nclon[:]  = sinfo["longitude"]; nclat[:] = sinfo["latitude"]
+    nctime[:] = (collect(1:length(dt)).-1) * 10;
+    nczwd[:]  = zwd; ncsig[:] = sig;
 
 end
