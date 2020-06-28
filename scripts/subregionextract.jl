0a6426ab2e412d0eb2aedaa58e43305bf4797eb1
Nathanael Wong <natgeo.wong@outlook.com>
2020-06-28 03:58:13 -0400

    Add subregionextract script

---
+++ scripts/subregionextract.jl
@@ -0,0 +1,19 @@
+using DrWatson
+@quickactivate "MaritimeContPWV"
+using ClimateERA
+using GeoRegions
+
+
+init,eroot = erastartup(aID=1,dID=1,path="/n/kuangdss01/lab/ecmwf/");
+erasubregion(init,eroot,modID="msfc",parID="tcw",regID="SEA",pregID="TRP");
+erasubregion(init,eroot,modID="msfc",parID="tcw",regID="SMT",pregID="TRP");
+erasubregion(init,eroot,modID="msfc",parID="tcwv",regID="SEA",pregID="TRP");
+erasubregion(init,eroot,modID="msfc",parID="tcwv",regID="SMT",pregID="TRP");
+
+init,eroot = erastartup(aID=1,dID=2,path="/n/kuangdss01/lab/ecmwf/");
+erasubregion(init,eroot,modID="msfc",parID="tcw",regID="SMT",pregID="SEA");
+erasubregion(init,eroot,modID="msfc",parID="tcwv",regID="SMT",pregID="SEA");
+
+# for yr = 2017 : 2019, mo = 1 : 12
+    # clisatdownload("mtpw2m",Date(yr,mo),regions=["TRP"],path="/n/kuangdss01/lab/clisat/");
+# end
