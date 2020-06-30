8aed543c9bd861184542e61c574c02ef8c77ec08
Nathanael Wong <natgeo.wong@outlook.com>
2020-06-30 19:25:25 -0400

    Changing root

--- scripts/downloads.jl
+++ scripts/downloads.jl
@@ -3,11 +3,11 @@ using DrWatson
 using ClimateERA
 # using ClimateSatellite
 
-init,eroot = erastartup(aID=1,dID=1,path="/n/kuangdss01/lab/ecmwf/");
+init,eroot = erastartup(aID=1,dID=1,path="/n/kuangdss01/lab/");
 # eradownload(init,eroot,modID="msfc",parID="tcw",regID="TRP");
 eradownload(init,eroot,modID="msfc",parID="tcwv",regID="TRP");
 
-init,eroot = erastartup(aID=1,dID=2,path="/n/kuangdss01/lab/ecmwf/");
+init,eroot = erastartup(aID=1,dID=2,path="/n/kuangdss01/lab/");
 eradownload(init,eroot,modID="msfc",parID="tcw",regID="SEA");
 eradownload(init,eroot,modID="msfc",parID="tcwv",regID="SEA");
 
