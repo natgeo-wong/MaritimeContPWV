a7a996216ec03e704310aa3c1c13da175b8d420d
Nathanael Wong <natgeo.wong@outlook.com>
2020-11-07 22:50:40 -0500

    Analyze CSF.

--- scripts/runcsf.jl
+++ scripts/runcsf.jl
@@ -6,4 +6,5 @@ include(srcdir("calccsf.jl")); adderaparams()
 for yr = 1979 : 2019
     init,eroot = erastartup(aID=2,dID=1,path="/n/kuangdss01/lab/",welcome=false)
     csf(init,eroot,eroot,regID="SEA",timeID=yr)
+    eraanalysis(init,eroot,modID="csfc",parID="csf",regID="SEA")
 end
