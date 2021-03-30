722d995a8ef090b1862be837a4c7d09956c0a191
Nathanael Wong <natgeo.wong@outlook.com>
2021-03-30 11:22:27 -0400

    Let's try this again.

--- src/eravgnss.jl
+++ src/eravgnss.jl
@@ -173,21 +173,29 @@ function era5vgnss_daily(
         if isfile(gfnc); ii += 1
 
             gds = Dataset(gfnc); gzwd = gds["zwd"][:]*1; close(gds);
-            gzwd = reshape(gzwd,144,:);
-            gzwd = dropdims(mean(gzwd,dims=1),dims=1);
+            gzwd = reshape(gzwd,144,:); ndy = size(gzwd,2)
+            gtmp = zeros(ndy)
+            for idy = 1 : ndy
+                gii = @view gzwd[:,idy]
+                gii = @view gii[.!isnan.(gii)]; nnan = length(gii)
+                if nnan >= 72
+                      gtmp[idy] = mean(gii)
+                else; gtmp[idy] = NaN
+                end
+            end
 
             pds,pvar = erarawread(pmod,ppar,preg,proot,date)
             ipi  = pvar[plon,plat,:]*1; close(pds)
             ipi  = reshape(ipi,24,:)
             ipi  = dropdims(mean(ipi,dims=1),dims=1);
-            gtcw = gzwd .* ipi * 1000
+            gtcw = gtmp .* ipi * 1000
 
             cds,cvar = erarawread(cmod,cpar,creg,croot,date)
             etcw = cvar[ilon,ilat,:]*1; close(cds)
             etcw = reshape(etcw,24,:)
             etcw = dropdims(mean(etcw,dims=1),dims=1);
 
-            irem = (.!ismissing.(gzwd)) .& (.!isnan.(gtcw));
+            irem = (.!ismissing.(gtmp)) .& (.!isnan.(gtmp));
             etcw = etcw[irem]; gtcw = gtcw[irem]
             gve[:,:,ii] = fit(Histogram,(gtcw,etcw),(gpwv,epwv)).weights
 
