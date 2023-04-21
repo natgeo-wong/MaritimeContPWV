cf4076ecae244b14f81047c08414e7a3043d3aa7
Nathanael Wong <natgeo.wong@outlook.com>
2023-04-21 12:32:51 -0400

    Renaming some scripts

--- scripts/runcsf.jl
+++ scripts/runcsf.jl
@@ -3,7 +3,7 @@ using DrWatson
 
 using ERA5Reanalysis
 
-include(srcdir("scwv.jl"))
+include(srcdir("csf.jl"))
 
 e5ds = ERA5Hourly(start=Date(2021),stop=Date(2021),path=datadir())
 egeo = ERA5Region("TRP",gres=0.25)
