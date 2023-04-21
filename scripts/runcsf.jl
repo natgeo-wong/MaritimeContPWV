ef2443556fed1846aef4f1cff22fb9fc7b484e45
Nathanael Wong <natgeo.wong@outlook.com>
2023-04-21 12:34:02 -0400

    Default should start from 1979 and end in 2021

--- scripts/runcsf.jl
+++ scripts/runcsf.jl
@@ -5,7 +5,7 @@ using ERA5Reanalysis
 
 include(srcdir("csf.jl"))
 
-e5ds = ERA5Hourly(start=Date(2021),stop=Date(2021),path=datadir())
+e5ds = ERA5Hourly(start=Date(1979),stop=Date(2021,12),path=datadir())
 egeo = ERA5Region("TRP",gres=0.25)
 
 calculatecsf(e5ds,egeo)
--- scripts/runscwv.jl
+++ scripts/runscwv.jl
@@ -5,7 +5,7 @@ using ERA5Reanalysis
 
 include(srcdir("scwv.jl"))
 
-e5ds = ERA5Hourly(start=Date(2021),stop=Date(2021),path=datadir())
+e5ds = ERA5Hourly(start=Date(1979),stop=Date(2021,12),path=datadir())
 egeo = ERA5Region("TRP",gres=0.25)
 
 calculatescwv(e5ds,egeo,p_top=10)
